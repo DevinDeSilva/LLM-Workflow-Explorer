d6bd6d6f638f11f211972bdbd520736b7bd928cd
Devin De Silva <desild@rpi.edu>
2026-05-06 01:43:20 -0400

    biomni results

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 source .venv/bin/activate
 
-#python baseline_FullContextAnswer.py --evaluation biomni-base
+# python baseline_FullContextAnswer.py --evaluation biomni-base
 # python baseline_LLMbased.py --evaluation biomni-base
 # python explainer_experiment.py --evaluation biomni-base
 # python baseline_VectorSimilarityAnswer.py --evaluation biomni-base
 # python baseline_HippoRAG.py --evaluation biomni-base
 # python baseline_HyperGraphRAG.py --evaluation biomni-base
-python explainer_experiment.py --evaluation biomni-base
+# python explainer_experiment.py --evaluation biomni-base
 
 # cd baselines/grasp
 # source .venv/bin/activate
@@ -14,7 +14,7 @@ python explainer_experiment.py --evaluation biomni-base
 # mkdir /home/desild/work/research/LLM-Workflow-Explorer/evaluations/biomni-base/explainer/grasp/exp_202604201325
 # grasp file configs/biomni-base.yaml   --input-file /home/desild/work/research/LLM-Workflow-Explorer/evaluations/biomni-base/ground_truth/ground_truth_data.jsonl   --output-file /home/desild/work/research/LLM-Workflow-Explorer/evaluations/biomni-base/explainer/grasp/exp_202604201325/RESULTS.jsonl   --progress
 
-cd /home/desild/work/research/LLM-Workflow-Explorer
-source .venv/bin/activate
+# cd /home/desild/work/research/LLM-Workflow-Explorer
+# source .venv/bin/activate
 python evaluation_results.py --evaluation biomni-base
-#python answer_winrate_evaluation.py --evaluation biomni-base
+python answer_winrate_evaluation.py --evaluation biomni-base
